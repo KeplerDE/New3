bf63d16de17d5fcdab317626dd8f15230acec80b
KeplerDE <90717368+KeplerDE@users.noreply.github.com>
2021-11-23 02:48:59 +0300

    #4
    Signed-off-by: KeplerDE <90717368+KeplerDE@users.noreply.github.com>

--- my_page/settings.py
+++ my_page/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'my_page.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [   BASE_DIR / 'horoscope' / 'templates'     ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
